8582409f1cf152cd3476151f3d352f47651f4181
Thanh Truong <thanhtruong@Thanhs-MacBook-Pro.local>
2020-01-26 17:31:23 +0700

    fix test app 2

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {});
+it("renders without crashing", () => {
+  const a = 1;
+  const b = 2;
+  const c = a + b;
+  expect(c).toBe(3);
+});
